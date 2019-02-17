58c1648705acb51a194d5c03d4539a80190e587b
bugaga6enika <vtomazov@gmail.com>
2019-02-17 00:20:22 +0000

    Application layer added Application: IoC added

---
+++ CarAdverts.Application/Configuratoins/IoC.cs
@@ -0,0 +1,21 @@
+﻿using CarAdverts.Domain.CarAdvert;
+using CarAdverts.Domain.Core.Persistence;
+using CarAdverts.Infrastructure.Contexts;
+using CarAdverts.Infrastructure.Repositories;
+using CarAdverts.Infrastructure.UnitOfWork;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace CarAdverts.Application.Configuratoins
+{
+    public static class IoC
+    {
+        public static void RegisterServices(IServiceCollection services)
+        {
+            services.AddDbContext<CarAdvertContext>();
+            services.AddScoped<ICarAdvertRepository, CarAdvertRepository>();
+            services.AddScoped<IUnitOfWork<CarAdvertContext, CarAdvert, Guid>, CarAdvertUnitOfWork>();
+            services.AddScoped<IContext<CarAdvert, Guid>, CarAdvertContext>();
+        }
+    }
+}
